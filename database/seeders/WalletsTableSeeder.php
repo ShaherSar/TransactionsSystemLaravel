3475166e271e3af183966f4fcd01a9dc2da4b951
Shaher Sarairah <shahersar@icloud.com>
2021-12-04 00:50:50 +0200

    Seeders Fixed

---
+++ database/seeders/WalletsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class WalletsTableSeeder extends Seeder{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+        DB::table('users')->insert([
+            'balance' => 0,
+            'user_id'=>1,
+            'created_at' => now(),
+            'updated_at'=> now(),
+        ]);
+    }
+}
